a6fdbb234ea1fc5771146ac823bf2436e0d9e1db
antspting <max.verzilov32@gmail.com>
2025-12-08 01:51:48 +0500

    feat: add data access layer with ApplicationDbContext and ApplicationUser models

---
+++ DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DataAccess.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Status> Statuses { get; set; }
+}
---
+++ DataAccess/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace DataAccess.Models;
+
+public class ApplicationUser : IdentityUser
+{
+    public int StatusId { get; set; }
+    public Status Status { get; set; }
+}
