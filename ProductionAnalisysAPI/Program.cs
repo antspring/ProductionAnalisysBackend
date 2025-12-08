55715805e2a83cd6b3ee1ccde73c24bf3866c908
antspting <max.verzilov32@gmail.com>
2025-12-08 17:14:18 +0500

    feat: integrate Entity Framework Core with PostgreSQL and configure identity API

--- ProductionAnalisysAPI/Program.cs
+++ ProductionAnalisysAPI/Program.cs
@@ -1,8 +1,17 @@
+using DataAccess;
+using DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSqlConnectionString")));
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<ApplicationUser>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
 
@@ -11,6 +20,8 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.MapIdentityApi<ApplicationUser>();
 
 app.UseHttpsRedirection();
 
