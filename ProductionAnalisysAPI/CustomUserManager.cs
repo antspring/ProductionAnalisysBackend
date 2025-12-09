400a6f6b7b806a9b886bfe4ea95526609a6dd159
antspting <max.verzilov32@gmail.com>
2025-12-10 01:07:08 +0500

    feat: add roles and seeder

---
+++ ProductionAnalisysAPI/CustomUserManager.cs
@@ -0,0 +1,36 @@
+﻿using DataAccess.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+
+namespace ProductionAnalisysAPI;
+
+public class CustomUserManager : UserManager<ApplicationUser>
+{
+    public CustomUserManager(
+        IUserStore<ApplicationUser> store,
+        IOptions<IdentityOptions> optionsAccessor,
+        IPasswordHasher<ApplicationUser> passwordHasher,
+        IEnumerable<IUserValidator<ApplicationUser>> userValidators,
+        IEnumerable<IPasswordValidator<ApplicationUser>> passwordValidators,
+        ILookupNormalizer keyNormalizer,
+        IdentityErrorDescriber errors,
+        IServiceProvider services,
+        ILogger<UserManager<ApplicationUser>> logger)
+        : base(store, optionsAccessor, passwordHasher,
+            userValidators, passwordValidators,
+            keyNormalizer, errors, services, logger)
+    {
+    }
+
+    public override async Task<IdentityResult> CreateAsync(ApplicationUser user, string password)
+    {
+        var result = await base.CreateAsync(user);
+
+        if (result.Succeeded)
+        {
+            await AddToRoleAsync(user, "Operator");
+        }
+
+        return result;
+    }
+}
--- ProductionAnalisysAPI/Program.cs
+++ ProductionAnalisysAPI/Program.cs
@@ -1,6 +1,9 @@
 using DataAccess;
 using DataAccess.Models;
+using DataAccess.Seeders;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using ProductionAnalisysAPI;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,12 +11,19 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSqlConnectionString")));
 builder.Services.AddAuthorization();
 builder.Services.AddIdentityApiEndpoints<ApplicationUser>()
+    .AddRoles<IdentityRole>()
+    .AddUserManager<CustomUserManager>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    await RoleSeeder.SeedAsync(scope.ServiceProvider);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
