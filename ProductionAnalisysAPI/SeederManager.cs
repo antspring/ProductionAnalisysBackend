31795893502c394026d01ae6eb6761802fdffe00
antspting <max.verzilov32@gmail.com>
2025-12-10 03:00:27 +0500

    refactor: move service collection in separate class

---
+++ ProductionAnalisysAPI/SeederManager.cs
@@ -0,0 +1,12 @@
+﻿using DataAccess.Seeders;
+
+namespace ProductionAnalisysAPI;
+
+public class SeederManager
+{
+    public static async Task SeedAsync(IServiceProvider serviceProvider)
+    {
+        using var scope = serviceProvider.CreateScope();
+        await RoleSeeder.SeedAsync(scope.ServiceProvider);
+    }
+}
---
+++ ProductionAnalisysAPI/ServiceCollectionExtension.cs
@@ -0,0 +1,30 @@
+﻿using DataAccess;
+using DataAccess.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Services.Services.Implementations;
+using Services.Services.Interfaces;
+
+namespace ProductionAnalisysAPI;
+
+public static class ServiceCollectionExtension
+{
+    public static void AddDbContext(this IServiceCollection services, ConfigurationManager configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("PostgreSqlConnectionString")));
+    }
+
+    public static void AddIdentity(this IServiceCollection services)
+    {
+        services.AddIdentityApiEndpoints<ApplicationUser>(options => options.Password.RequiredLength = 8)
+            .AddRoles<IdentityRole>()
+            .AddUserManager<CustomUserManager>()
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+    }
+
+    public static void AddDependencyInjectionServices(this IServiceCollection services)
+    {
+        services.AddSingleton<IPasswordGenerateService, PasswordGenerateService>();
+    }
+}
