fb9f57d48987edae653443173ec8fb3203a5accd
antspting <max.verzilov32@gmail.com>
2026-01-16 22:44:15 +0500

    feat: add hourly several analysis model and migration

---
+++ Domain/Models/ProductionAnalysis/HourlySeveral/HourlySeveral.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Models.Catalogs;
+using Domain.Models.ProductionDownTime;
+
+namespace Domain.Models.ProductionAnalysis.HourlySeveral;
+
+public class HourlySeveral
+{
+    public int Id { get; init; }
+
+    public int Product1Id { get; set; }
+    public CatalogValue Product1 { get; set; } = null!;
+
+    public int Product2Id { get; set; }
+    public CatalogValue Product2 { get; set; } = null!;
+
+    public int DepartmentId { get; set; }
+    public CatalogValue Department { get; set; } = null!;
+
+    public int PerformerId { get; set; }
+    public CatalogValue Performer { get; set; } = null!;
+
+    public int ShiftId { get; set; }
+    public CatalogValue Shift { get; set; } = null!;
+
+    public int WorkHourId { get; set; }
+    public CatalogValue WorkHour { get; set; } = null!;
+
+    public DateOnly Date { get; set; }
+
+    public double CycleTime1 { get; set; }
+    public double CycleTime2 { get; set; }
+
+    public double DailyRate1 { get; set; }
+    public double DailyRate2 { get; set; }
+
+    public double Fact { get; set; }
+
+    public double Changeover { get; set; }
+
+    public int ProductionDocumentId { get; set; }
+    public ProductionDocument ProductionDocument { get; set; } = null!;
+}
--- Infrastructure/ApplicationDbContext.cs
+++ Infrastructure/ApplicationDbContext.cs
@@ -2,6 +2,7 @@
 using Domain.Models.Catalogs;
 using Domain.Models.ProductionAnalysis.HourlyByPower;
 using Domain.Models.ProductionAnalysis.HourlyByTactTime;
+using Domain.Models.ProductionAnalysis.HourlySeveral;
 using Domain.Models.ProductionDownTime;
 using Infrastructure.EntityTypeConfigurations;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
@@ -25,6 +26,7 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     public DbSet<HourlyByTactTimeView> HourlyByTactTimeViews { get; set; }
     public DbSet<HourlyByPower> HourlyByPower { get; set; }
     public DbSet<HourlyByPowerView> HourlyByPowerViews { get; set; }
+    public DbSet<HourlySeveral> HourlySeveral { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -45,5 +47,6 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
         modelBuilder.ApplyConfiguration(new HourlyByTactTimeViewConfiguration());
         modelBuilder.ApplyConfiguration(new HourlyByPowerConfiguration());
         modelBuilder.ApplyConfiguration(new HourlyByPowerViewConfiguration());
+        modelBuilder.ApplyConfiguration(new HourlySeveralConfiguration());
     }
 }
